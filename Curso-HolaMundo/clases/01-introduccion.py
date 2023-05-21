405808fffe87c5875d22b77fbac5af3df7a43acc
mich43ll <prueba@gmail.com>
2023-05-21 09:11:16 -0600

    .

---
+++ Curso-HolaMundo/clases/01-introduccion.py
@@ -0,0 +1,8 @@
+mensaje = "hola mundo"
+print(type(mensaje))
+
+#clase: es el plano de construccion
+#objeto: instancia de una clase
+
+#clases: el plano de construccion de una casa 
+#objeto: la casa construida 
